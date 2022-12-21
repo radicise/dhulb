b5b26d0842d405afb9fc8bc8aadc2cbc97ab9a45
Tristan <tschartman41@gmail.com>
2022-12-21 13:02:06 -0500

    Create Preprocessor.java

---
+++ src/Dhulb/Preprocessor.java
@@ -0,0 +1,10 @@
+package Dhulb;
+
+import java.nio.file.Path;
+
+public class Preprocessor {
+    // takes file contents and the directory path where the files are stored and from which relative paths should be constructed
+    public static String processData (String data, Path cwd) throws Exception {
+        return "";
+    }
+}
