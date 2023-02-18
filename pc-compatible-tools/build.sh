3ba77349397491f87e5cc3ca12733f13bd7709e5
Tristan <73963573+tsj845@users.noreply.github.com>
2023-02-17 23:01:35 -0500

    compile

--- pc-compatible-tools/build.sh
+++ pc-compatible-tools/build.sh
@@ -2,7 +2,7 @@
 set -e
 cp boot.s boot-inter.s
 cat io.s >> boot-inter.s
-dhulbpp - - < test.dhulb | dhulbc 16 -tNGT >> boot-inter.s
+./dhulbpp - - < test.dhulb | ./dhulbc 16 -tNGT >> boot-inter.s
 as -arch i386 -m16 -o boot.o boot-inter.s
 strip boot.o
 FILEOFF=$(otool -l boot.o | grep "fileoff" | grep -oE -m 1 '[^ ]+$')
