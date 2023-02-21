7c38a0cdebc75bb76cce9882495edfeaaccb4638
radicise <93229379+radicise@users.noreply.github.com>
2023-02-20 20:38:34 -0500

    fixes

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@
 set -ue
 cd ${1}
 DHULB_PATHNAME="$(pwd)"
+mkdir -p bin
 printf "Usage: The argument is the path of the Dhulb home directory\n"
 printf "LibPath=${DHULB_PATHNAME}/src/DLib\nExtPath=${DHULB_PATHNAME}/src/DExt\nDefPath=${DHULB_PATHNAME}/targets/linux_i386.dhulb_def\n" > ~/.dhulb_conf
 printf "~/.dhulb_conf has been regenerated\n"
