1c7760fe157460dff581de6f752634d555957264
radicise <93229379+radicise@users.noreply.github.com>
2023-02-20 20:40:25 -0500

    another fix

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -ue
 printf "Usage: The argument is the directory of the Dhulb compiler source repository\n"
 cd ${1}
+mkdir -p bin
 cd src
 javac -d ../bin Dhulb/Dhulb.java
 javac -d ../bin Dhulb/Preprocessor.java
