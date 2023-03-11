54174168718aaaaff7a2f910acb1196e27676657
radicise <93229379+radicise@users.noreply.github.com>
2023-03-11 06:19:46 -0500

    Update sys.s

--- src/DLib/linux-i386/sys.s
+++ src/DLib/linux-i386/sys.s
@@ -5,18 +5,18 @@ incl %eax
 int $0x80
 read:/*dhulbDoc-v300:function;s32 read(s32, a32, u32) call32;*/
 pushl %ebx
-movl 8(%ebp),%ebx
-movl 12(%ebp),%ecx
-movl 16(%ebp),%edx
+movl 8(%esp),%ebx
+movl 12(%esp),%ecx
+movl 16(%esp),%edx
 movl $0x03,%eax
 int $0x80
 popl %ebx
 retl
 write:/*dhulbDoc-v300:function;s32 write(s32, a32, u32) call32;*/
 pushl %ebx
-movl 8(%ebp),%ebx
-movl 12(%ebp),%ecx
-movl 16(%ebp),%edx
+movl 8(%esp),%ebx
+movl 12(%esp),%ecx
+movl 16(%esp),%edx
 movl $0x04,%eax
 int $0x80
 popl %ebx
